135b2b293ff8f3c2c4a500ebcc00c4a312353df9
Fhikar Zul <Fhikarzul18@gmail.com>
2022-08-29 10:02:42 +0800

    Initial Commit

---
+++ SplitView/SplitViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitViewApp.swift
+//  SplitView
+//
+//  Created by Lucy on 29/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SplitViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
